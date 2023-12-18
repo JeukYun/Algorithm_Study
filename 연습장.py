ac74791365d279cfd677808d70256e379a8746a5
JeUkYun <yju1205@gmail.com>
2023-12-18 22:46:25 +0900

    알고리즘 기초(1)

--- 연습장.py
+++ 연습장.py
@@ -1,24 +1,13 @@
-# N = int(input())
-# S = list(map(int, input().split()))
+a = ['42', '31', '16' ,'19']
+# print(a[0][0])
 
-# occur = [0 for _ in range(200001)] 
-# for i in range(N):
-#     # abs 함수는 인자로 들어온 수의 절댓값을 반환하는 함수입니다.
-#     occur[abs(S[i])] += 1
+# a = [5, 4, 3, 2, 1]
 
-# print(occur)
+# print(a.pop('min(a)'))
+# print(a)
 
-# ans = 0
-# for i in range(1, 200001):
-#     if occur[i] != 1:
-#         continue
-#     for j in range(N):
-#         if abs(S[j]) == i:
-#             ans += S[j]
-        
-# print(ans)
+lst = []
+for i in range(len(a)):
+    lst.append(int(a[i][0]))
+    lst.min(a)
 
-# print((10+2)%10)
-
-lst = [1, 2, 3, 4]
-print(lst[-1])
