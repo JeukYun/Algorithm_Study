a5a1c8308d06cdd1a9f708813e80c2ac20bc34a7
JeUkYun <https://github.com/JeukYun>
2024-02-06 23:02:16 +0900

    heap예제

--- Udemy_강의/01_자료구조/test/03_11286.py
+++ Udemy_강의/01_자료구조/test/03_11286.py
@@ -1,1 +1,25 @@
+# 자료 정렬시 튜플도 정렬이 가능하다.
+'''
+a = [(1,1), (3,2), (2,3), (1,3), (2,1)]
+a.sort() 실행 후 a 출력시
+[(1,1), (1,3), (2,1), (2,3), (3,2)]
+
+'''
+
 import sys
+import heapq as hq
+
+n = int(sys.stdin.readline())
+
+pq = []
+
+for _ in range(n):
+    s = int(sys.stdin.readline())
+    if s: # 입력값이 0이 아닌경우
+        hq.heappush(pq, (abs(s), s)) # 리스트에 (s절대값, s) 튜플삽입
+    else: # 입력값이 0인경우
+        if pq:
+            print(hq.heappop(pq)[1])
+        else:
+            print('0')
+
