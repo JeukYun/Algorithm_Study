2d9c279a2e18adc726f4bb142f6f41e999c471a4
JeUkYun <yju1205@gmail.com>
2024-02-11 14:24:21 +0900

    알고리즘 풀이_map

--- Udemy_강의/01_자료구조/test/04_1302.py
+++ Udemy_강의/01_자료구조/test/04_1302.py
@@ -1,12 +1,24 @@
 import sys
 
+d= {}
+
 n = int(sys.stdin.readline())
 
-top_list = []
-
 for _ in range(n):
-    title = sys.stdin.readline()
-    # print(title)
+    book = sys.stdin.readline().rstrip()
 
-    top_list.append(title)
+    if book in d:
+        d[book] += 1
+    else:
+        d[book] = 1
 
+max_val = max(d.values())
+
+max_b = []
+
+for k,v in d.items():
+    # print(k, v)
+    if v == max_val:
+        max_b.append(k)
+
+print(sorted(max_b)[0])
