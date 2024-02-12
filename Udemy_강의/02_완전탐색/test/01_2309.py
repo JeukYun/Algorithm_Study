c9d1d8e65caab499499ded6eaf9996622551ffdc
JeUkYun <yju1205@gmail.com>
2024-02-12 22:06:11 +0900

    문제풀이 수정

--- Udemy_강의/02_완전탐색/test/01_2309.py
+++ Udemy_강의/02_완전탐색/test/01_2309.py
@@ -1,24 +1,20 @@
 from itertools import combinations
+# 중복 비허용이므로 combinations
 
-lst = []
+# lst = []
+# for _ in range(9):
+#     lst.append(int(input()))
 
-for _ in range(9):
-    lst.append(int(input()))
+# 아래와 같이 단순화 가능
+lst = [int(input()) for _ in range(9)]
 
+for comb in combinations(lst, 7):
+    if sum(comb) == 100: # 튜플의 합이 100인경우
 
-sum_lst = []
-for _ in combinations(lst, 7):
-    sum_lst.append(_)
+        # 튜플 정렬함수 : sorted()
+        for i in sorted(comb):
+            print(i)
+        break # break가 없는경우, 중복답을 모두 출력하게 되므로 break 필수
 
-for lst in sum_lst:
-    if sum(lst) == 100:
-        lst = list(lst)
-        lst.sort(reverse = True)
-        # print(lst)
-        break
-
-for _ in range(len(lst)):
-    # print(lst)
-    print(lst.pop())
 
 
