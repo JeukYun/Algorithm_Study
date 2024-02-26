e252f82edc528eb9efda646db64f6251f1738d33
JeUkYun <yju1205@gmail.com>
2024-02-26 16:58:28 +0900

    그리디 풀이

--- Udemy_강의/03_그리디/03_1213.py
+++ Udemy_강의/03_그리디/03_1213.py
@@ -1,23 +1,41 @@
-STR = input()
+txt = list(input())
+txt.sort()
+
+# 알파벳의 갯수를 카운트
+dic = {}
+for i in txt:
+    dic[i] = 0
+for i in txt:
+    dic[i] += 1
+
+lst = []
+# 문자, 문자 갯수 리스트에 담기
+for k, v in dic.items():
+    lst.append((k, v))
+    
+
+palindrome = []
+# 홀수개 문자 저장
+not_odd = []
 
 
-txt = ''.join(sorted(STR)) # 입력된 문자
-txt_set = ''.join(sorted(set(txt))) # 중복이 제거된 문자
-
-print(txt)
-print(txt_set)
-
-if not len(txt) % 2:
-    if len(txt_set) * 2 != len(txt):
-        print("I'm Sorry Hansoo")
+for i in lst:
+    # 짝수개인 경우
+    if not i[1] % 2:
+        palindrome.append(i[0] * int(i[1]/2)) # 문자수 /2만큼 저장
     else:
-        print(txt_set + txt_set[::-1])
+        not_odd.append(i[0])
+        palindrome.append(i[0] * int((i[1] - 1)/2))
+
+# 홀수개 문자가 1개인 경우 
+if len(not_odd) == 1:
+    print("".join(palindrome + not_odd + palindrome[::-1])) 
+
+# 홀수개 문자가 없는 경우
+elif len(not_odd) == 0:
+    print("".join(palindrome + palindrome[::-1]))
+
+# 그 외 (홀수개 문자 2이상...)
 else:
-    if (len(txt_set)-1) * 2 != (len(txt)-1): 
-        print("I'm Sorry Hansoo")
-    else:
-        print()
-
-new = []
-for i in range(len(txt)):
-    new.append()
+    print("I'm Sorry Hansoo")
+       
