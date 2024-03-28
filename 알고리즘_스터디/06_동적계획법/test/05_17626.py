28c7c3f26bf6fdaacd26f55b1ffaa396f2323bab
JeUkYun <yju1205@gmail.com>
2024-03-28 16:54:59 +0900

    DP 풀이

--- 알고리즘_스터디/06_동적계획법/test/05_17626.py
+++ 알고리즘_스터디/06_동적계획법/test/05_17626.py
@@ -15,8 +15,9 @@ for i in range(1, n + 1):
     j = 1
     while j*j <= i:
         if DP[i] == 0: # DP에 값이 저장되어있지 않은 경우
-            DP[i] = DP[j*j] + DP[i - j*j] 
-        else:
+            DP[i] = DP[j*j] + DP[i - j*j]
+
+        else: # DP에 값이 저장된 경우 DP[j*j] + DP[i - j*j]와 비교하여 더 작은값으로 업데이트
             DP[i] = min(DP[i], DP[j*j] + DP[i - j*j])
         
         j += 1
